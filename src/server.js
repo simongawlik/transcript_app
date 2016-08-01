64cbf4e4c25b219db9d05eddb88aebaf28318d3b
Simon Gawlik <sgawlik@gmail.com>
2016-08-01 15:35:09 -0400

    very basic server handling routing

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 
+// 404 response
+function send404Response(response) {
+	response.writeHead(404, {"Content-type": "text/plain"});
+	response.write("Error 404: Page not found!");
+	response.end();
+}
+
+
+// handle user request
 function onRequest(request, response) {
-	console.log("A user made a request" + request.url);
-	response.writeHead(200, {"Context-Type": "text/plain"});
-	response.write("Here is some data");
-	response.end(); 
+	if (request.method == 'GET' && request.url == '/') {
+		response.writeHead(200, {"Content-type": "text/html"});
+		fs.createReadStream("./index.html").pipe(response);
+	} else {
+		send404Response(response);
+	}
 }
 
 http.createServer(onRequest).listen(8888);
